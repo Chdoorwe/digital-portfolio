d45df019f2a0d1c31d6179e3534f9d03c2245e10
Chdoorwe <122927338+Chdoorwe@users.noreply.github.com>
2026-03-30 08:46:10 -0400

    started to add more stuff ot the Puzzels

---
+++ resorsis/js/puzel/VisibilityPuzels.js
@@ -0,0 +1,9 @@
+    function updateVisibility(condition, elementId) {
+        if (condition) {
+        elementId.style.display = "block";   // show
+    } else {
+        elementId.style.display = "none";    // hide
+    }
+
+  
+}
